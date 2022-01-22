1f74eb801f68d63081f5bbdfa2afbb889fe3ea76
La7rodectus <maxmega27@gmail.com>
2022-01-22 06:14:28 +0200

    kostyl prodaktion

--- src/socket/handlers/acceptSDP.js
+++ src/socket/handlers/acceptSDP.js
@@ -2,7 +2,7 @@ const events = require('../events.js')
 
 function action(args) {
   const { peerId, sessionDescription } = args
-
+  fastify.log.warn(`send sdp type ${JSON.stringify(sessionDescription.type)} from ${JSON.stringify(socket.id)} to ${JSON.stringify(peerId)}`)
   fastify.io.to(peerId).emit(events.SESSION_DESCRIPTION, {
     peerId: socket.id,
     sessionDescription,
