82355e5484d248a92f3a55788ffde1a7c4592521
La7rodectus <maxmega27@gmail.com>
2022-01-22 07:15:09 +0200

    add from field

--- src/socket/handlers/acceptSDP.js
+++ src/socket/handlers/acceptSDP.js
@@ -1,10 +1,11 @@
 const events = require('../events.js')
 
 function action(args) {
-  const { peerId, sessionDescription } = args
+  const { peerId, from, sessionDescription } = args
   fastify.log.warn(`send sdp type ${JSON.stringify(sessionDescription.type)} from ${JSON.stringify(socket.id)} to ${JSON.stringify(peerId)}`)
   fastify.io.to(peerId).emit(events.SESSION_DESCRIPTION, {
     peerId: socket.id,
+    from,
     sessionDescription,
   });
 
